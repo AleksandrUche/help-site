9855a2a724f2a2765b58a608ccc623d1daecf311
AleksandrUche <zak.reizer@mail.ru>
2023-05-03 23:37:42 +0300

    added a model of corners according to GOST

--- calculator_weight/models.py
+++ calculator_weight/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class CornerEqualShelvesGost(models.Model):
+    name = models.CharField(verbose_name='Размер', max_length=10)
+    side = models.DecimalField(verbose_name='Сторона уголка, a', max_digits=5, decimal_places=1)
+    thickness = models.DecimalField(verbose_name='Толщина, t', max_digits=5, decimal_places=1)
+    weight = models.FloatField(verbose_name='Масса, 1м/кг')
