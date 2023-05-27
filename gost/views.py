1273b14229cfad0254e63f43db95e930c67eb13a
AleksandrUche <zak.reizer@mail.ru>
2023-05-27 12:57:49 +0300

    added gost 28759.3-2022 and docstrings

--- gost/views.py
+++ gost/views.py
@@ -9,6 +9,7 @@ import importlib
 
 
 def load_class_db(name_cls):
+    """Загружает однотипные модели"""
     module_path = 'gost.models'
     module = importlib.import_module(module_path)
     my_class = getattr(module, name_cls)
@@ -16,13 +17,14 @@ def load_class_db(name_cls):
 
 
 class DocumentsView(TemplateView):
+    """Начальная страница стандартов"""
     template_name = 'gost/documents.html'
     extra_context = {'title': 'Стандарты'}
 
 
-# Отображение поиска фланцев по ГОСТ 33259-2015
 class Gost33259View(View):
-    template_name = 'gost/gost33259.html'
+    """Представление для фланцев ГОСТ 33259-2015"""
+    template_name = 'gost/gost_33259_flange.html'
     form_class = Gost33259Form
 
     def get(self, request):
@@ -45,15 +47,15 @@ class Gost33259View(View):
     def post(self, request):
         form = self.form_class(request.POST)
         # Значения которые нужно вывести при выборе ряда 1 и ряда 2 (уплотнительная поверхность)
-        values_surface_fl = {
-            1: {
+        fields_surface_fl = {
+            '1': {
                 'B': ['h_lower', 'd2'],
                 'C': ['h1_lower', 'd3_row_one', 'd4_row_one'],
                 'D': ['h_lower', 'h2_lower', 'd2', 'd6_row_one', 'd5_row_one'],
                 'E': ['h1_lower', 'd4_row_one'],
                 'F': ['h_lower', 'h2_lower', 'd2', 'd6_row_one'],
             },
-            2: {
+            '2': {
                 'B': ['h_lower', 'd2'],
                 'C': ['h1_lower', 'd3_row_two', 'd4_row_two'],
                 'D': ['h_lower', 'h2_lower', 'd2', 'd6_row_two', 'd5_row_two'],
@@ -61,7 +63,29 @@ class Gost33259View(View):
                 'F': ['h_lower', 'h2_lower', 'd2', 'd6_row_two'],
             }
         }
-
+        # Значения которые нужно вывести при выборе ряда 1 и ряда 2 (типы фланцев)
+        fields_type_fl = {
+            'Gost33259Type01': {
+                '1': ['dn_passage', 'pn', 'dv_lower_row_one', 'b_lower_row_one', 'c1_lower', 'd_row_one', 'd1',
+                      'd_lower_row_one', 'n_lower_row_one', 'pin_row_one'],
+                '2': ['dn_passage', 'pn', 'dv_lower_row_two', 'b_lower_row_two', 'c1_lower', 'd_row_two', 'd1',
+                      'd_lower_row_two', 'n_lower_row_two', 'pin_row_two']
+            },
+            'Gost33259Type02': {
+                '1': ['dn_passage', 'pn', 'd0_row_one', 'd2', 'dv_lower_row_one', 'b_lower_row_one', 'b1_lower_row_one',
+                      'c_lower_row_one', 'c1_lower', 'd_row_one', 'd1', 'd_lower_row_one', 'n_lower_row_one',
+                      'pin_row_one'],
+                '2': ['dn_passage', 'pn', 'd0_row_two', 'd2', 'dv_lower_row_two', 'b_lower_row_two', 'b1_lower_row_two',
+                      'c_lower_row_two', 'c1_lower', 'd_row_two', 'd1', 'd_lower_row_two', 'n_lower_row_two',
+                      'pin_row_two']
+            },
+            'Gost33259Type11': {
+                '1': ['dn_passage', 'pn', 'dm_row_one', 'dn_row_one', 'd1_lower_row_one', 'b_lower_row_one',
+                      'h_row_one', 'h1', 'd_row_one', 'd1', 'd_lower_row_one', 'n_lower_row_one', 'pin_row_one'],
+                '2': ['dn_passage', 'pn', 'dm_row_two', 'dn_row_two', 'd1_lower_row_two', 'b_lower_row_two',
+                      'h_row_two', 'h1', 'd_row_two', 'd1', 'd_lower_row_two', 'n_lower_row_two', 'pin_row_two']
+            }
+        }
         if form.is_valid():
             type_fl = form.cleaned_data['type_fl']
             surface_fl = form.cleaned_data['surface']
@@ -70,61 +94,106 @@ class Gost33259View(View):
             row = form.cleaned_data['row']
 
             objects_types_fl = load_class_db(f'Gost33259Type{type_fl}')
-
+            # Чертеж определенного типа фланца
             drawing_flange_type = Gost33259TypeDrawing.objects.filter(type_fl=type_fl)
+            # Чертеж уплотнительной поверхности
             drawing_flange_surface = Gost33259SurfaceDrawing.objects.filter(surface_fl=surface_fl)
+            # Значения для определенного типа фланца
             flange_data = objects_types_fl.objects.filter(dn_passage=dn_passage, pn=pn)
+            # Значения уплотнительной поверхности
             surface_data = Gost33259SurfaceValues.objects.filter(dn_passage=dn_passage, pn=pn)
-            if pn != '250':  # в ГОСТ есть давление 250, но массы на них нет
-                if pn == '2.5':
-                    pn = '2'
-                mass_flange = Gost33259Mass.objects.filter(dn_passage=dn_passage,
-                                                           type_fl=type_fl).values_list(f'pn_{pn}', flat=True).get()
-            else:
-                mass_flange = None
-            # необходимые поля из БД для отображения в шаблоне
-            fields_surface = values_surface_fl[row][surface_fl]
+            if pn == '2.5':
+                pn = '2'
+            try:
+                mass_flange = Gost33259Mass.objects.filter(dn_passage=dn_passage, type_fl=type_fl, ).values_list(
+                    f'pn_{pn}', flat=True).get()
+            except:
+                mass_flange = None  # При возвращении None в шаблоне будет отображено "Масса не указана"
 
-            # Gost33259Type01  ['dn_passage', 'pn', 'dv_lower_row_one', 'dv_lower_row_two', 'b_lower_row_one', 'b_lower_row_two',
-            # 'c1_lower', 'd_row_one', 'd_row_two', 'd1', 'd_lower_row_one', 'd_lower_row_two', 'n_lower_row_one',
-            # 'n_lower_row_two', 'pin_row_one', 'pin_row_two']
-
-            # Gost33259Type02['dn_passage', 'pn', 'd0_row_one', 'd0_row_two', 'd2', 'dv_lower_row_one', 'dv_lower_row_two',
-            #  'b_lower_row_one', 'b_lower_row_two', 'b1_lower_row_one', 'b1_lower_row_two', 'c_lower_row_one',
-            #  'c_lower_row_two', 'c1_lower', 'd_row_one', 'd_row_two', 'd1', 'd_lower_row_one', 'd_lower_row_two',
-            #  'n_lower_row_one', 'n_lower_row_two', 'pin_row_one', 'pin_row_two']
-
-            # Gost33259Type11
-            # ['dn_passage', 'pn', 'dm_row_one', 'dm_row_two', 'dn_row_one', 'dn_row_two', 'd1_lower_row_one',
-            #  'd1_lower_row_two', 'b_lower_row_one', 'b_lower_row_two', 'h_row_one', 'h_row_two', 'h1', 'd_row_one',
-            #  'd_row_two', 'd1', 'd_lower_row_one', 'd_lower_row_two', 'n_lower_row_one', 'n_lower_row_two',
-            #  'pin_row_one', 'pin_row_two']
-            fields_type = [field.name for field in objects_types_fl._meta.get_fields()][1:]
-
-            return render(request, self.template_name,
-                          context={'form': form,
+            return render(request,
+                          self.template_name,
+                          context={'form': self.form_class,
                                    'flange_data': flange_data,
                                    'drawing_flange_type': drawing_flange_type,
                                    'drawing_flange_surface': drawing_flange_surface,
-                                   'fields_surface': fields_surface,
-                                   'fields_type': fields_type,
+                                   # поля для уплотнительной поверхности (для отображения таблицы в шаблоне)
+                                   'fields_surface': fields_surface_fl[row][surface_fl],
+                                   # поля для фланца (для отображения таблицы в шаблоне)
+                                   'fields_type': fields_type_fl[f'Gost33259Type{type_fl}'][row],
                                    'surface_data': surface_data,
                                    'mass_flange': mass_flange,
+                                   },
+                          )
+        return render(request, self.template_name, context={'form': self.form_class})
+
+
+class Gost28759View(View):
+    """Представление для фланцев ГОСТ 28759.3—2022"""
+    template_name = 'gost/gost_28759_flange.html'
+    form_class = Gost28759Form
+
+    def get(self, request):
+        if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+            dn_passage = request.GET.get('dn_passage')
+            if dn_passage:
+                pn_option = list(
+                    Gost28759FlangeValues.objects.filter(dn_passage=dn_passage).values_list('pn', flat=True)
+                )
+                return JsonResponse({'pn_option': pn_option}, status=200)
+
+        return render(request, self.template_name, context={'form': self.form_class})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            execution = form.cleaned_data['execution']
+            dn_passage = form.cleaned_data['dn_passage']
+            pn = form.cleaned_data['pn']
+
+            # Значения исполнения
+            flange_data = Gost28759FlangeValues.objects.filter(dn_passage=dn_passage, pn=pn)
+            # Чертеж исполнения
+            drawing_flange_execution = Gost28759FlangeDrawing.objects.filter(execution_fl=execution)
+            # Масса фланца
+            try:
+                mass_flange = Gost28759FlangeMass.objects.filter(dn_passage=dn_passage, pn=pn).values_list(
+                    f'exec_{execution}', flat=True).get()
+            except:
+                mass_flange = None
+
+            # необходимые поля для определенного исполнения из БД
+            fields_execution = {
+                '1': ['dn_passage', 'pn', 'd1', 'd2', 'd3', 'd4', 'd6', 'd7', 's', 'b_lower', 'h', 'd_lower', 'pin',
+                      'quantity_pin'],
+                '2': ['dn_passage', 'pn', 'd1', 'd2', 'd5', 'd6', 'd7', 's', 'b_lower', 'h', 'd_lower', 'pin',
+                      'quantity_pin'],
+                '3': ['dn_passage', 'pn', 'd1', 'd2', 'd3', 'd4', 'd6', 'd7', 's', 'b_lower', 'h', 'a_lower', 'd_lower',
+                      'pin', 'quantity_pin'],
+                '4': ['dn_passage', 'pn', 'd1', 'd2', 'd5', 'd6', 'd7', 's', 'b_lower', 'h', 'a1_lower', 'd_lower',
+                      'pin', 'quantity_pin'],
+                '5': ['dn_passage', 'pn', 'd1', 'd2', 'd3', 'd4', 'd6', 'd7', 's', 'b_lower', 'h', 'd_lower', 'pin',
+                      'quantity_pin'],
+                '6': ['dn_passage', 'pn', 'd1', 'd2', 'd5', 'd6', 'd7', 's', 'b_lower', 'h', 'd_lower', 'pin',
+                      'quantity_pin'],
+            }
+            return render(request, self.template_name,
+                          context={'form': form,
+                                   'flange_data': flange_data,
+                                   'drawing_flange_execution': drawing_flange_execution,
+                                   'mass_flange': mass_flange,
+                                   'fields_exec': fields_execution[execution],
                                    }
                           )
         return render(request, self.template_name, context={'form': form})
 
 
-'''Отображение поиска фланцев по АТК 26-18-13-96'''
-
-
 class Atk261813View(View):
+    """Представление для фланцев АТК 26-18-13-96"""
     template_name = 'gost/atk261813.html'
     form_class = Atk261813Form
 
     def get(self, request):
-        form = self.form_class
-
         if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
             execution = request.GET.get('execution')
             pn = request.GET.get('pn')
@@ -138,7 +207,7 @@ class Atk261813View(View):
                 dn_passage_option = list(objects_exec_fl.objects.filter(pn=pn).values_list('dn_passage', flat=True))
                 return JsonResponse({'dn_passage_option': dn_passage_option}, status=200)
 
-        return render(request, self.template_name, context={'form': form})
+        return render(request, self.template_name, context={'form': self.form_class})
 
     def post(self, request):
         form = self.form_class(request.POST)
@@ -163,16 +232,12 @@ class Atk261813View(View):
         return render(request, self.template_name, context={'form': form})
 
 
-'''Отображение поиска Днищ по ГОСТ 6533-78'''
-
-
 class Gost6533View(View):
-    template_name = 'gost/gost6533.html'
+    """Представление для днищ по ГОСТ 6533-78"""
+    template_name = 'gost/gost_6533_bottom.html'
     form_class = Gost6533Form
 
     def get(self, request):
-        form = self.form_class
-
         if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
             drawing = request.GET.get('drawing')
             diameter = request.GET.get('diameter')
@@ -186,7 +251,7 @@ class Gost6533View(View):
                 thickness_option = list(Gost6533Bottoms.objects.filter(d=diameter).values_list('s_lower', flat=True))
                 return JsonResponse({'thickness_option': thickness_option}, status=200)
 
-        return render(request, self.template_name, context={'form': form})
+        return render(request, self.template_name, context={'form': self.form_class})
 
     def post(self, request):
         form = self.form_class(request.POST)
@@ -209,16 +274,12 @@ class Gost6533View(View):
         return render(request, self.template_name, context={'form': form})
 
 
-'''Заглушки АТК 24.200.02-90 (обычные)'''
-
-
 class Atk24200FlangeStoppersView(View):
+    """Представление для заглушек АТК 24.200.02-90 (обычные)"""
     template_name = 'gost/atk24200FlangeStoppers.html'
     form_class = Atk24200FlangeStoppersForm
 
     def get(self, request):
-        form = self.form_class
-
         if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
             execution = request.GET.get('execution')
             pn = request.GET.get('pn')
@@ -232,7 +293,7 @@ class Atk24200FlangeStoppersView(View):
                 dn_passage_option = list(objects_exec_fl.objects.filter(pn=pn).values_list('dn_passage', flat=True))
                 return JsonResponse({'dn_passage_option': dn_passage_option}, status=200)
 
-        return render(request, self.template_name, context={'form': form})
+        return render(request, self.template_name, context={'form': self.form_class})
 
     def post(self, request):
         form = self.form_class(request.POST)
@@ -257,16 +318,12 @@ class Atk24200FlangeStoppersView(View):
         return render(request, self.template_name, context={'form': form})
 
 
-'''Заглушки ПОВОРОТНЫЕ АТК 26-18-5-93'''
-
-
 class Atk26185FlangeStoppersView(View):
+    """Представление для заглушек АТК 26-18-5-93 (поворотные)"""
     template_name = 'gost/atk2618FlangeStoppers.html'
     form_class = Atk26185FlangeStoppersForm
 
     def get(self, request):
-        form = self.form_class
-
         if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
             execution = request.GET.get('execution')
             pn = request.GET.get('pn')
@@ -280,7 +337,7 @@ class Atk26185FlangeStoppersView(View):
                 dn_passage_option = list(objects_exec_fl.objects.filter(pn=pn).values_list('dn_passage', flat=True))
                 return JsonResponse({'dn_passage_option': dn_passage_option}, status=200)
 
-        return render(request, self.template_name, context={'form': form})
+        return render(request, self.template_name, context={'form': self.form_class})
 
     def post(self, request):
         form = self.form_class(request.POST)
