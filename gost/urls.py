eb86f9ff119a5389ef1663291d6b47d154fdbe6c
AleksandrUche <zak.reizer@mail.ru>
2023-12-13 23:03:38 +0300

    added URLs for ajax requests

--- gost/urls.py
+++ gost/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 from .views import *
+from .services.ajax_services import (
+    get_form_values_gost_33259,
+    get_form_values_atk_261813,
+    get_form_values_gost_28759,
+    get_form_values_atk_24200_flange_stoppers,
+    get_form_values_atk_26185_flange_stoppers,
+    get_form_values_gost_6533,
+)
 
 urlpatterns = [
     path('', DocumentsView.as_view(), name='documents'),  # начальная страница документов
@@ -10,3 +18,20 @@ urlpatterns = [
     path('atk-26-18-5-93/', Atk26185FlangeStoppersView.as_view(), name='atk26185'),  # заглушки АТК поворотные
     path('gost-6533-78/', Gost6533View.as_view(), name='gost6533'),  # днища
 ]
+
+ajax_urls = [
+    path('gost-33259-2015/get-values-form/', get_form_values_gost_33259, name='get_values_gost_33259'),
+    path('atk-26-18-13-96/get-values-form/', get_form_values_atk_261813, name='get_values_atk_261813'),
+    path('gost-28759-3-2022/get-values-form/', get_form_values_gost_28759, name='get_values_gost_28759'),
+    path('atk-24-200-02-90/get-values-form/',
+         get_form_values_atk_24200_flange_stoppers,
+         name='get_values_atk_24200_flange_stoppers'
+         ),
+    path('atk-26-18-5-93/get-values-form/',
+         get_form_values_atk_26185_flange_stoppers,
+         name='get_values_atk_26185_flange_stoppers'
+         ),
+    path('gost-6533-78/get-values-form/', get_form_values_gost_6533, name='get_values_gost_6533'),
+]
+
+urlpatterns.extend(ajax_urls)
